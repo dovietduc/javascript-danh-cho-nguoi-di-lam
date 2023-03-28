a22cce2c54e89b557276f70b278a287c85550a83
ducdo <vietduc122@gmail.com>
2023-03-28 13:07:19 +0700

    buoi 2

--- buoi-1/index.js
+++ buoi-1/index.js
@@ -1,40 +1,7 @@
-// nguyen thuy
+let x = 10;
+let y = x;
 
-// 1. Kiểu dữ liệu undefined
-console.log(typeof(undefined));
-let x;
-console.log(x);
+y = 12;
 
-// 2. kiểu dữ liệu boolean
-console.log(typeof(true));
-console.log(typeof(false));
-let isTrue = true;
-let isFalse = false;
-
-console.log(typeof(isTrue));
-console.log(typeof(isFalse));
-
-
-// Kiểu dữ liệu number
-console.log(typeof(123));
-console.log(typeof(123.45));
-let numberInt = 123;
-let numberFloat = 123.45;
-
-console.log(typeof(numberInt));
-
-
-// Kiểu dữ liệu string;
-console.log(typeof("ducdo"));
-console.log(typeof('ducdo'));
-console.log(typeof(`ducdo`));
-let nameInfo = "ducdo";
-console.log(typeof(nameInfo));
-
-// kiểu dữ liệu null
-console.log(typeof(null));
-
-
-
-
-// khong phai nguyen thuy
+console.log('x', x);
+console.log('y', y);
