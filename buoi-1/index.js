d7d0d7a8b99b81f85e2e1011afb404fcb28acbed
ducdo <vietduc122@gmail.com>
2023-03-28 10:55:48 +0700

    1. kieu du lieu nguyen thuy

---
+++ buoi-1/index.js
@@ -0,0 +1,40 @@
+// nguyen thuy
+
+// 1. Kiểu dữ liệu undefined
+console.log(typeof(undefined));
+let x;
+console.log(x);
+
+// 2. kiểu dữ liệu boolean
+console.log(typeof(true));
+console.log(typeof(false));
+let isTrue = true;
+let isFalse = false;
+
+console.log(typeof(isTrue));
+console.log(typeof(isFalse));
+
+
+// Kiểu dữ liệu number
+console.log(typeof(123));
+console.log(typeof(123.45));
+let numberInt = 123;
+let numberFloat = 123.45;
+
+console.log(typeof(numberInt));
+
+
+// Kiểu dữ liệu string;
+console.log(typeof("ducdo"));
+console.log(typeof('ducdo'));
+console.log(typeof(`ducdo`));
+let nameInfo = "ducdo";
+console.log(typeof(nameInfo));
+
+// kiểu dữ liệu null
+console.log(typeof(null));
+
+
+
+
+// khong phai nguyen thuy
---
+++ buoi-1/redmind.md
@@ -0,0 +1,12 @@
+Kiểu dữ liệu nguyên thuỷ
+
+    + undefined
+    + null
+    + Numbers
+    + string
+    + Booleans
+    + BigInts
+    + Symbols
+
+Kiểu dữ liệu object
+ 
