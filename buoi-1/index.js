3359bdbaf921d3be852d7e589b9bd88a77037e3f
ducdo <vietduc122@gmail.com>
2023-03-29 15:30:36 +0700

    buoi 10

--- buoi-1/index.js
+++ buoi-1/index.js
@@ -1,2 +1,15 @@
-let nameInfor = "ducdo";
-let nameInforClone = nameInfor;
+// let a = {};
+// let b = {};
+// console.log(a === b); // true
+
+// let a = 5;
+// let b = "5";
+
+// console.log(a === b);
+
+const ducInfor = {
+    name: "duc"
+};
+
+ducInfor.name = "dung";
+// ducInfor = "dung";
